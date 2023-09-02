a87b725a6256544f4e73986e06175fdf0e3cd998
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-09-02 15:24:35 +0900

    create HeroPage and MainNavigation
     - HeroPage 구현
     - MainNavigation 구현
     - add styled-components

---
+++ components/hero.js
@@ -0,0 +1,50 @@
+"use client";
+import styled from "styled-components";
+import Image from "next/image";
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 7rem);
+`;
+
+const ImageBox = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  div {
+    font-size: 2.5rem;
+    position: absolute;
+    left: 40%;
+    top: 50%;
+  }
+`;
+
+const Hero = () => {
+  return (
+    <Box>
+      <ImageBox>
+        <Image
+          src="/images/hero_sample.png"
+          alt="Hero 이미지 입니다."
+          width={1440}
+          height={900}
+        ></Image>
+        <div>
+          <span style={{ color: "#ff0d64" }}>즐겁고</span>
+          <span> 쉽게 일지를 </span>
+          <span style={{ color: "#ff0d64" }}>CHECK</span>
+          <span> 하자! </span>
+        </div>
+      </ImageBox>
+    </Box>
+  );
+};
+
+export default Hero;
---
+++ components/icons/person.js
@@ -0,0 +1,12 @@
+const Person = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="none">
+      <path
+        fill="#000"
+        d="M22.5 22.5c4.144 0 7.5-3.356 7.5-7.5s-3.356-7.5-7.5-7.5A7.498 7.498 0 0 0 15 15c0 4.144 3.356 7.5 7.5 7.5Zm0 3.75c-5.006 0-15 2.512-15 7.5v3.75h30v-3.75c0-4.988-9.994-7.5-15-7.5Z"
+      />
+    </svg>
+  );
+};
+
+export default Person;
